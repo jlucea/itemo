6b95ab4e9a8e3dc69cd53ea7c45bd6b49ab05777
Jaime Lucea <jlucea.ios@gmail.com>
2025-04-17 22:03:43 +0200

    Initial Commit

---
+++ Itemo/ItemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItemoApp.swift
+//  Itemo
+//
+//  Created by Jaime Lucea on 17/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ItemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
